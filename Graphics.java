7e69b5ed13ae96f80b88de061709371171872b3d
jlc029 <jlc029@uark.edu>
2014-04-06 17:52:27 -0500

    Graphics

---
+++ Graphics.java
@@ -0,0 +1,31 @@
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+@SuppressWarnings("serial")
+public class Graphics extends JPanel
+{
+	public void paintComponent(java.awt.Graphics g)
+	{
+		super.paintComponent(g);
+		ImageIcon i = new ImageIcon("C:\\Users\\Samsung\\workspace\\Game\\src\\brick room.png");
+		i.paintIcon(this, g, 600, 0);
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		Graphics g = new Graphics();
+		JFrame f = new JFrame();
+		f.setSize(800, 600);
+		f.setVisible(true);
+		f.add(g);
+	}
+	
+}
+	

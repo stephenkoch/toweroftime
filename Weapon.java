2dc3a4bf52be5b714a51b53703dbf2e518614262
BrandonConover <bjconove@uark.edu>
2014-04-17 22:45:17 -0500

    Weapon slot updated to string value

--- Weapon.java
+++ Weapon.java
@@ -2,17 +2,17 @@
 public class Weapon extends Equipment{
 	protected int damage;
 	protected int speed;
-	Weapon() {
-		
-	}
+	Weapon() {}
 	
-	Weapon(String name, String desc, int slot, int damage, int speed) {
+	Weapon(String name, String desc, String slot, int speed, int damage) 
+	{
 		super.name = name;
 		super.description = desc;
 		super.slot = slot;
-		this.damage = damage;
 		this.speed = speed;
+		this.damage = damage;
 	}
-	public int getWeaponDamage(){return this.damage;}
+	
 	public int getWeaponSpeed(){return this.speed;}
+	public int getWeaponDamage(){return this.damage;}
 }

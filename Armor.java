645811c7d4c4c1e2b918176b250606232fa46ac9
BrandonConover <bjconove@uark.edu>
2014-04-17 22:38:34 -0500

    Minor Change to slots
    Changed Slot from int to String

--- Armor.java
+++ Armor.java
@@ -1,10 +1,9 @@
-
 public class Armor extends Equipment{
 	protected int defense;
 	
 	Armor() {}
 	
-	Armor(String name, String desc, int slot, int defense) {
+	Armor(String name, String desc, String slot, int defense) {
 		super.name = name;
 		super.description = desc;
 		super.slot = slot;
